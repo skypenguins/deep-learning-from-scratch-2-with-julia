356114602ff183852fc138197caa2df957fd7b25
skypenguins <monomeg.lab@gmail.com>
2020-09-26 14:19:18 +0900

    Added abstract type

--- common/functions.jl
+++ common/functions.jl
@@ -1,26 +1,28 @@
 module Functions
 export sigmoid, relu, softmax, cross_entropy_error
 
-function sigmoid(x)
+abstract type Func end
+
+function sigmoid(x) <: Func
     return 1 ./ (1 .+ exp.(-x))
 end
 
-function relu(x)
+function relu(x) <: Func
     return max(0, x) # NOT maximum()
 end
 
 # ifで分岐せず，多重ディスパッチを利用
-function softmax(x::Array{Any,2})
+function softmax(x::Array{Any,2}) <: Func
     x = x .- maximum(x, dims=2) # Juliaではインデックスは1始まり
     return exp.(x) ./ sum(x, dims=2)
 end
 
-function softmax(x::Array{Any,1})
+function softmax(x::Array{Any,1}) <: Func
     x = x .- maximum(x)
     return exp.(x) ./ sum(x) 
 end
 
-function cross_entropy_error(y, t)
+function cross_entropy_error(y, t) <: Func
     if dims(y) == 1
         t = reshape(t, 1, :)
         y = reshape(y, 1, :)
