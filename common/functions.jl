4ae80b61ab397d06f6d34b40dcef8a0b17e95eae
skypenguins <monomeg.lab@gmail.com>
2020-09-28 02:56:58 +0900

    Modified softmax()

--- common/functions.jl
+++ common/functions.jl
@@ -1,18 +1,17 @@
 module Functions
 export sigmoid, relu, softmax, cross_entropy_error
 
-abstract type Func end
-
-function sigmoid(x) <: Func
+function sigmoid(x)
     return 1 ./ (1 .+ exp.(-x))
 end
 
-function relu(x) <: Func
+function relu(x)
     return max(0, x) # NOT maximum()
 end
 
 #= バッチ対応版ソフトマックス関数
 softmax(a::Array{Float64,2})::Array{Float64,2} = begin
+    @show size(a)
     y = []
     for row in 1:size(a, 1)
         c = maximum(a[row, :])
@@ -24,31 +23,33 @@ end
 
 cross_entropy_error(y::Array{Float64,2}, t::Array{Float64,2})::Float64 = -sum(t .* log.(y .+ 1e-7)) =#
 
-function softmax(x) <: Func
-    if ndims(x) == 2
-        x = x .- maximum(x, dims=2) # Juliaではインデックスは1始まり
-        x = exp.(x) ./ sum(x, dims=2)
-    elseif ndims(x) == 1
-        x = x .- maximum(x)
-        x = exp.(x) ./ sum(x) 
-    end
-    
-    return x
-end 
+softmax(x) = x
+function softmax(x::Vector{<:Real})
+    x = x .- maximum(x, dims=2) # Juliaではインデックスは1始まり
+    x .= exp.(x) / sum(exp.(x), dims=2) # .= は高速化
+end
 
-function cross_entropy_error(y, t) <: Func
-    if dims(y) == 1
+function softmax(x::Matrix{<:Real})
+    x = x .- maximum(x)
+    x .= exp.(x) ./ sum(exp.(x)) 
+end
+
+function cross_entropy_error(y, t)
+    # TODO:普通に数式の通りに計算するバージョンを検討
+    # ミニバッチ学習の処理と共通化させるため，Vectorの場合は1xN Arrayにreshapeする
+    if ndims(y) == 1
         t = reshape(t, 1, :)
         y = reshape(y, 1, :)
     end
 
-    # 教師データがont-hotベクトルの場合，正解ラベルのインデックスに変換
+    # 教師データがont-hotベクトルの場合，yのt列目を取得をするため正解ラベルのインデックスに変換
     if length(t) == length(y) # 要素数
-        t = argmax(t, dims=2)
+        t = argmax(t, dims=2)[1][1]
     end
 
     batch_size = size(y, 1)
 
-    return -sum(log(y[collect(1:batch_size), t] + 1e-7)) ./ batch_size
+    # collect()はインデックス番号用，yのt列目を取得
+    return .-(sum(log.(y[collect(1:batch_size), t] .+ 1e-7)) ./ batch_size)
 end
 end
