52edc3f25b0a9764619d6ad149966410672c23ea
skypenguins <monomeg.lab@gmail.com>
2020-10-10 20:32:02 +0900

    Added Adam

--- common/optimizer.jl
+++ common/optimizer.jl
@@ -17,3 +17,43 @@ function update!(self::SGD, params, grads)
         end
     end
 end
+
+mutable struct Adam <: Optim
+    #= Adam
+    (http://arxiv.org/abs/1412.6980v8) =#
+    lr
+    β_1
+    β_2
+    iter
+    m
+    v
+    function Adam(;lr=0.001, β_1=0.9, β_2=0.999)
+        self = new()
+        self.lr = lr
+        self.β_1 = β_1
+        self.β_2 = β_2
+        self.iter = 0
+        self.m = nothing
+        self.v = nothing
+        return self
+    end
+end
+
+function update!(self::Adam, params, grads)
+    if self.m === nothing
+        self.m, self.v = [], []
+        for param = params
+            push!(m, zeros(size(param)...))
+            push!(v, zeros(size(param)...))
+        end
+    end
+    self.iter += 1
+    lr_t = self.lr .* sqrt(1.0 - self.β_2^self.iter) / (1.0 - self.β_1^self.iter)
+
+    for i = 1:length(params)
+        self.m[i] .+= (1 - self.β_1) * (grads[i] - self.m[i])
+        self.v[i] .+= (1 - self.β_2) * (grads[i]^2 - self.v[i])
+
+        params[i] .-= lr_t .* self.m[i] / (sqrt(self.v[i]) .+ 1e-7)
+    end
+end
